c8e998101ef0216f709a314ec1a79b44e9c0444a
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2022-12-11 21:35:40 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -1,8 +1,31 @@
 SELECT * FROM students;
+
 SELECT * FROM students WHERE Age > 30;
+
 SELECT * FROM students WHERE Age > 30 And Gender="F";
+
 SELECT Points From students WHERE Name="Alex";
+
 INSERT INTO students(Name,Age, Gender, Points) VALUES ("Ahmad", 22, "M", 250);
+
 UPDATE SET students Points = Points + 200 WHERE Name = "Basma";
+
 UPDATE SET students Points = Points - 50 WHERE Name = "Alex";
+
+
+CREATE TABLE graduates(
+		ID Integer NotNull Primarykey Auto Incerement,
+		Name Text NotNull Unique,
+		Age Integer,
+		Gender Text,
+		Points Integer,
+		Graduation, TextPoints,
+);
+
+INSERT INTO graduates(Name, Age, Gender, Points)
+		SELECT Name, Age, Gender, Points FROM students WHERE Name = "Layal";
+		
+UPDATE graduates SET Graduation = "08/09/2018" WHERE Name = "Layal";
+	
+DELETE FROM students WHERE Name = "Layal";
 
