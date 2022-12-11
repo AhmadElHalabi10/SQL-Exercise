d109a8dca20a5883ea3ab6fa4d2a2fdb66684744
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2022-12-11 21:50:01 +0200

    Joins

--- answers.sh
+++ answers.sh
@@ -29,3 +29,15 @@ UPDATE graduates SET Graduation = "08/09/2018" WHERE Name = "Layal";
 	
 DELETE FROM students WHERE Name = "Layal";
 
+
+CREATE TABLE newtable AS
+	SELECT employees.Name, companies.Name, companies.Date
+	FROM employees JOIN companies
+	On employees.Company = companies.Name;
+	
+SELECT * FROM employees WHERE Date <2000;
+SELECT * FROM Company WHERE Role = "graphic designer"
+
+
+
+
